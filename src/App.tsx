d0dcf238459bd4b162dc1e051976015fbb7fbd41
Nora Hamida <hamida.nora@gmail.com>
2024-12-05 10:01:32 +0100

    Creation d'un fichier routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
-import Home from './pages/Home'
-import APropos from './pages/APropos'
-import NotFound from './pages/NotFound'
-import FicheLogement from './pages/FicheLogement'
 
 import './styles/App.scss'
 import './styles/index.scss'
 import './styles/mainLayout.scss'
+import { routes } from './routes'
 
 function App() {
   return (
     <Router>
       <MainLayout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/a-propos" element={<APropos />} />
-          <Route path="/ficheLogement/:id" element={<FicheLogement />} />
-          <Route path="*" element={<NotFound />} />
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </MainLayout>
     </Router>
