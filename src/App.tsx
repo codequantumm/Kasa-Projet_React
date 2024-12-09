88e7b18912c34a0bfaa47049bf99a8d8ba8dafa6
Nora Hamida <hamida.nora@gmail.com>
2024-12-09 12:55:34 +0100

    fix: suppression de la création de root en double dans app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 
@@ -21,11 +19,5 @@ function App() {
     </Router>
   )
 }
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
 
 export default App
